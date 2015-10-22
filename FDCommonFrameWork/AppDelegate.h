ed5a7bce43659ee83f6fd9e9d373ebf369afffc8
icemilk00 <442377973@qq.com>
2015-10-22 10:53:56 +0800

    Initial Commit

---
+++ FDCommonFrameWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FDCommonFrameWork
+//
+//  Created by hp on 15/10/22.
+//  Copyright (c) 2015年 hxp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FDCommonFrameWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FDCommonFrameWork
+//
+//  Created by hp on 15/10/22.
+//  Copyright (c) 2015年 hxp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
